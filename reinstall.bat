a52088cf2537fa2ecb655ed5753aa9a07e3ed356
CoolColdUK <35877983+CoolColdUK@users.noreply.github.com>
2020-02-21 23:54:53 +0000

    Create reinstall.bat

---
+++ reinstall.bat
@@ -0,0 +1,20 @@
+choco install 7zip
+choco install bulkrenameutility
+choco install fsviewer
+choco install firefox
+choco install openoffice
+
+choco install notepadplusplus
+choco install anaconda3
+choco install vscode
+choco install nodejs
+choco install yarn
+choco install git
+choco install github-desktop
+choco install lunacy
+
+
+choco install handbrake
+choco install vlc
+choco install k-litecodecpackmega
+choco install mp3tag
